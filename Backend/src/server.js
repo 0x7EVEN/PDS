5c07b36871b81b44bcbf16976024b82b1b32b73a
Sushanth PS <getsushanthps@gmail.com>
2021-11-20 20:01:49 +0530

    cors added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,29 +1,31 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
-const db = require('./configs/db.config');
-const authController = require('./controllers/auth.controller');
-const centralController = require('./controllers/central.controller');
-const userController = require('./controllers/user.controller');
-const storeController = require('./controllers/store.controller');
-const warehouseController = require('./controllers/warehouse.controller');
+const db = require("./configs/db.config");
+const authController = require("./controllers/auth.controller");
+const centralController = require("./controllers/central.controller");
+const userController = require("./controllers/user.controller");
+const storeController = require("./controllers/store.controller");
+const warehouseController = require("./controllers/warehouse.controller");
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authController);
-app.use('/central', centralController);
-app.use('/user', userController);
-app.use('/store', storeController);
-app.use('/warehouse', warehouseController)
+app.use("/auth", authController);
+app.use("/central", centralController);
+app.use("/user", userController);
+app.use("/store", storeController);
+app.use("/warehouse", warehouseController);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, async () => {
-  try {
-    // connect to db
-    await db();
-    console.log('Server started!');
-  } catch (e) {
-    console.log('Error while connecting database');
-  }
+    try {
+        // connect to db
+        await db();
+        console.log("Server started!");
+    } catch (e) {
+        console.log("Error while connecting database");
+    }
 });
