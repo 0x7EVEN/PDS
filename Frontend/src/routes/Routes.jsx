dcfc413b0086685382baca31c75697f2bebca252
Sushanth PS <getsushanthps@gmail.com>
2021-11-21 00:22:04 +0530

    cart page

--- Frontend/src/routes/Routes.jsx
+++ Frontend/src/routes/Routes.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
+import Cart from "../components/Cart";
 import Dashboard from "../components/Dashboard";
 import Login from "../components/Login";
 import Store from "../components/Store";
+import Success from "../components/Success";
 
 export default function Links() {
     return (
@@ -9,6 +11,8 @@ export default function Links() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/store/:id" element={<Store />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/success" element={<Success />} />
         </Routes>
     );
 }
