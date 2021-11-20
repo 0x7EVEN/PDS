e7681c2544ec9da33ab906031b107bc73db2c029
Chiranjeev <6258860@gmail.com>
2021-11-21 04:14:35 +0530

    resolved store update issue

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -57,7 +57,7 @@ router.post('/checkout', protect, async (req, res) => {
     const { quota } = user;
 
     // Transaction that happened
-    const transaction = req.body.cart;
+    const transaction = req.body;
 
     // Wallet amount left
     let updatedWallet = Number(user.wallet);
@@ -70,7 +70,6 @@ router.post('/checkout', protect, async (req, res) => {
 
       // const value = Number(transaction[name]?.split('kg')[0]);
       const value = Number(transaction[name].split('kg')[0]);
-
 
       const updatedItem = {
         name: name,
@@ -97,7 +96,6 @@ router.post('/checkout', protect, async (req, res) => {
       { new: true }
     );
 
-
     if (!user) {
       return res
         .status(500)
@@ -108,7 +106,7 @@ router.post('/checkout', protect, async (req, res) => {
     let store = await Store.findById(req.body.store);
 
     const updateInventory = store.inventory.map(
-      ({ name, remaining, img, used }) => {
+      ({ name, remaining, img, used, supplied, received }) => {
         let captureRemaining = Number(remaining.split('kg')[0]);
         let captureUsed = Number(used.split('kg')[0]);
         const newItem = {
@@ -116,6 +114,8 @@ router.post('/checkout', protect, async (req, res) => {
           img: img,
           remaining: remaining,
           used: used,
+          supplied: supplied,
+          received: received,
         };
 
         const value = Number(transaction[name]?.split('kg')[0]);
@@ -143,6 +143,118 @@ router.post('/checkout', protect, async (req, res) => {
     });
   } catch (e) {
     console.log(e.message);
+    return res.status(500).json({ message: 'Error while checking out...' });
+  }
+});
+
+router.post('/checkout2', protect, async (req, res) => {
+  try {
+    let user = await User.findOne({ aadhaar: req.user.aadhaar });
+
+    // Quota left of user
+    const { quota } = user;
+
+    // Transaction that happened
+    const transaction = req.body;
+
+    // Wallet amount left
+    let updatedWallet = Number(user.wallet);
+
+    // Updating quota to reflect on database
+    const updatedQuota = quota.map(({ name, remaining, used, price }) => {
+      const captureRemaining = Number(remaining.split('kg')[0]);
+      const captureUsed = Number(used.split('kg')[0]);
+      const capturePrice = Number(price.split('/kg')[0]);
+
+      // const value = Number(transaction[name]?.split('kg')[0]);
+      let value = 0;
+      transaction.map((item) => {
+        if (item.name === name) {
+          value += Number(item.quantity);
+        }
+      });
+      // const value = Number(transaction[name].split('kg')[0]);
+
+      const updatedItem = {
+        name: name,
+        remaining: remaining,
+        used: remaining,
+        price: price,
+      };
+
+      if (value) {
+        updatedWallet -= String(capturePrice * value);
+        updatedItem['remaining'] = String(captureRemaining - value) + 'kg';
+        updatedItem['used'] = String(captureUsed + value) + 'kg';
+      }
+
+      return updatedItem;
+    });
+
+    user = await User.findByIdAndUpdate(
+      user._id,
+      {
+        quota: [...updatedQuota],
+        wallet: updatedWallet,
+      },
+      { new: true }
+    );
+
+    if (!user) {
+      return res
+        .status(500)
+        .json({ message: "Error while updating user's cart" });
+    }
+
+    for await (const item of req.body) {
+      let store = await Store.findById(item.store);
+
+      const updateInventory = store.inventory.map(
+        ({ name, remaining, img, used, supplied, received }) => {
+          let captureRemaining = Number(remaining.split('kg')[0]);
+          let captureUsed = Number(used.split('kg')[0]);
+
+          const newItem = {
+            name: name,
+            img: img,
+            remaining: remaining,
+            used: used,
+            supplied: supplied,
+            received: received,
+          };
+
+          // const value = Number(transaction[name]?.split('kg')[0]);
+          let value = 0;
+          transaction.map((el) => {
+            if (el.name === name && store._id.toString() === el.store) {
+              value += Number(el.quantity);
+            }
+          });
+
+          if (value) {
+            newItem['remaining'] = String(captureRemaining - value) + 'kg';
+            newItem['used'] = String(captureUsed + value) + 'kg';
+          }
+
+          return newItem;
+        }
+      );
+
+
+      store = await Store.findByIdAndUpdate(
+        store._id,
+        { inventory: [...updateInventory] },
+        { new: true }
+      );
+    }
+
+    // alert if stocks fells below 20% for each category
+
+    return res.status(200).json({
+      message: 'Success!',
+      user: user,
+    });
+  } catch (e) {
     return res.status(500).json({ message: 'Error while checking out...' });
   }
 });
