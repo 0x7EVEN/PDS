5aafc07746171de89a6d21a03108c7480170ad27
Chiranjeev <6258860@gmail.com>
2021-11-21 12:42:18 +0530

    added mongo_uri

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./contexts/AuthContestProvider";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContestProvider';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AuthContextProvider>
-                <App />
-            </AuthContextProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
